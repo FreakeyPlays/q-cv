6f16fb57511cafd62d7974608266174fc8287751
Leto01 <80838862+Leto01@users.noreply.github.com>
2022-05-12 22:45:09 +0200

    Copied a files from tix-108 to prepare for the GUI-Issues of the sub-tixs in this tix. Fetches data from server and puts it into an Array now for diplay or edits.

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,20 @@
 import React from "react";
+import Menu from "./components/menu/Menu";
+
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import ITSkills from "./pages/itskills/ITSkills";
+
+import "./App.css";
 
 const App = () => {
     return(
-        <>
-        </>
+        <Router>
+            <Menu>
+                <Routes>
+                    <Route path="/it-skills" element={<ITSkills />} />
+                </Routes>
+            </Menu>
+        </Router>
     )
 }
 
---
+++ client/src/pages/itskills/ITSkills.js
@@ -0,0 +1,13 @@
+import getAllSkillsFromDB from './ITSkills.items.js'
+
+const ITSkills = () => {
+
+    getAllSkillsFromDB();
+    return(
+        <>
+            <h1>IT-Skills</h1>
+        </>
+    )
+}
+
+export default ITSkills;
