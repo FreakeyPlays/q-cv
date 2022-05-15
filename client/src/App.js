05775203e148d5a1f8fb5fa112fbf0da9f17034a
FreakeyPlays <chrismerck@gmx.de>
2022-05-15 15:39:47 +0200

    Added new Routes to Create and Copy Issue #31 Task #35

--- client/src/App.js
+++ client/src/App.js
@@ -11,18 +11,21 @@ import Projects from "./pages/projects/Projects";
 import CreateCV from "./pages/createcv/CreateCV";
 
 import "./App.css"
+import CreateProject from "./pages/projects/CreateProject";
 
 const App = () => {
     return (
         <Router>
             <Menu>
                 <Routes>
-                    <Route path="/" exact element={<Dashboard />} />
+                    <Route path="/" index element={<Dashboard />} />
                     <Route path="/user-data" element={<UserData />} />
                     <Route path="/education" element={<Education />} />
                     <Route path="/career" element={<Career />} />
                     <Route path="/it-skills" element={<ITSkills />} />
-                    <Route path="/projects" element={<Projects />} />
+                    <Route path="projects" element={<Projects />} />
+                    <Route path="projects/create" element={<CreateProject />} />
+                    <Route path="projects/create/:id" element={<CreateProject />} />
                     <Route path="/create-cv" element={<CreateCV />} />
                 </Routes>
             </Menu>
