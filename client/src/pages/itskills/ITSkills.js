84b34a23964b3ac1e86fb9cd0216020f7d9183ac
Leto01 <80838862+Leto01@users.noreply.github.com>
2022-05-16 21:26:58 +0200

    basic adding of a new Skill done, needs a msg for the user to know if it was added.

--- client/src/pages/itskills/ITSkills.js
+++ client/src/pages/itskills/ITSkills.js
@@ -3,12 +3,40 @@ import { skillDataService } from '../../services/skills.services.js';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faPen, faPlus, faTrash} from '@fortawesome/free-solid-svg-icons'
 import Popup from '../../components/popup/Popup.js';
+import TextField from '@material-ui/core/TextField'
 import "./ITSkills.css";
+import { Button } from '@material-ui/core';
+
+import { MuiThemeProvider, createTheme } from '@material-ui/core/styles';
+
 
 const ITSkills = () => {
     const[buttonPopup, setButtonPopup] = useState(false);
+    const[newSkillName, setNewSkillName] = useState('');
+    //const[newSkillAddedToDB, setNewSkillAddedToDB] = useState(false);
+
     const [skill, setSkill] = useState([]);
     const receivedData = useRef(false);
+
+    const theme = createTheme({
+        palette: {
+            secondary: {
+                main: '#333'
+            }
+          },
+    });
+
+
+    const handleSubmit = (e) =>{
+        e.preventDefault();
+        if(newSkillName){
+            skillDataService.newSkill({
+                "name":newSkillName
+            })
+            //setNewSkillAddedToDB(true);
+            receivedData.current = false;
+        }
+    }
 
     useEffect( ()=>{
         if(receivedData.current === false){
@@ -42,7 +70,22 @@ const ITSkills = () => {
             </div>
             
             <Popup trigger ={buttonPopup}  setTrigger={setButtonPopup}>
-                <h3>MY POPUP</h3>
+                <h3>New Skill</h3>
+                    <form noValidate autoComplete='off' onSubmit={handleSubmit}>
+                        <MuiThemeProvider theme={theme}>
+                            <TextField 
+                            label='required' 
+                            required
+                            color='secondary'
+                            onChange={ (e) => setNewSkillName(e.target.value)} 
+                            ></TextField>
+                            <Button 
+                            type='submit'
+                            color='secondary'
+                            variant='contained'
+                            >Submit</Button>
+                        </MuiThemeProvider>
+                    </form>
             </Popup>
         </>
     )
