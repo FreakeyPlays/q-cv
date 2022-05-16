e2cd35e45a98e1825308bbcf42c456638f81ee87
Leto01 <80838862+Leto01@users.noreply.github.com>
2022-05-17 00:13:06 +0200

    Names are updateable over a popupwindow

--- client/src/pages/itskills/ITSkills.js
+++ client/src/pages/itskills/ITSkills.js
@@ -1,13 +1,45 @@
 import { useState, useEffect, useRef } from 'react';
 import { skillDataService } from '../../services/skills.services.js';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faPen, faPlus, faTrash} from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPen, faPlus, faTrash} from '@fortawesome/free-solid-svg-icons';
+import Popup from '../../components/popup/Popup.js';
+import TextField from '@material-ui/core/TextField'
 import "./ITSkills.css";
+import { Button } from '@material-ui/core';
+
+
+import { MuiThemeProvider, createTheme } from '@material-ui/core/styles';
 
 const ITSkills = () => {
     
     const [skill, setSkill] = useState([]);
     const receivedData = useRef(false);
+    
+    const[updatePopup, setUpdatePopup] = useState(false);
+    const[updateTargetId, setUpdateTargetId] = useState('');
+    const[newUpdateSkillName, setNewUpdateSkillName] = useState('');
+    const[oldUneditedName, setOldUneditedName] = useState('');
+    
+    const handleUpdateSubmit = (e) =>{
+        e.preventDefault();
+        if(updateTargetId){
+            skillDataService.updateSkill(updateTargetId,{
+                "name":newUpdateSkillName
+            }).then((res)=>{
+                console.log(res.params);
+                window.location.reload(false);
+                receivedData.current = false;
+            })
+        }
+    }
+
+    const theme = createTheme({
+        palette: {
+            secondary: {
+                main: '#333'
+            }
+          },
+    });
 
     useEffect( ()=>{
         if(receivedData.current === false){
@@ -30,11 +62,10 @@ const ITSkills = () => {
                             {item.name} 
                             <div className='interaction'>
                             <div data={index} onClick={ (e) =>  {
-                                        let id = skill[e.currentTarget.getAttribute("data")]._id;
-                                        skillDataService.updateSkill(id).then(res => {
-                                            window.location.reload(false);
-                                            receivedData.current = false;
-                                        })
+                                        setUpdateTargetId(skill[e.currentTarget.getAttribute("data")]._id);
+                                        console.log("Item with id has been clicked: " + updateTargetId);
+                                        setUpdatePopup(true);
+                                        setOldUneditedName(skill[e.currentTarget.getAttribute("data")].name);
                                     }
                                 }>
                                     <FontAwesomeIcon className='skillIcon' icon={faPen} />
@@ -51,6 +82,27 @@ const ITSkills = () => {
             }
             </div>
             <div className='addSkillIcon'><FontAwesomeIcon className='skillPlusIcon' icon={faPlus} /></div>
+            <Popup trigger={updatePopup} setTrigger={setUpdatePopup}>
+                                        <h3>Rename Skill</h3>
+                                        <form noValidate autoComplete='off' onSubmit={handleUpdateSubmit}>
+                                            <MuiThemeProvider theme={theme}>
+                                                <TextField 
+                                                label='required' 
+                                                required
+                                                color='secondary'
+                                                defaultValue={oldUneditedName}
+                                                variant="filled"
+                                                onChange={ (e) => setNewUpdateSkillName(e.target.value)}
+                                                ></TextField>
+                                                <Button 
+                                                type='submit'
+                                                color='secondary'
+                                                variant='contained'
+                                                >change</Button>
+                                            </MuiThemeProvider>
+                                        </form>
+                                    </Popup>
+                                    
         </>
     )
 }
