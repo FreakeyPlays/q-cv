01d3586e24df088e5c908c724b4fa50cc7f6ede4
mastit20 <mastit20@hs-esslingen.de>
2022-06-09 17:23:12 +0200

    Added Path into Client API call

--- client/src/services/user.services.js
+++ client/src/services/user.services.js
@@ -1,6 +1,8 @@
 import axios from "axios";
 
-const basePath = "http://localhost:5000/api/user"
+const ADDRESS = process.env.REACT_APP_URL;
+const PORT = process.env.REACT_APP_API_PORT;
+const basePath = "http://" + ADDRESS + ":" + PORT + "/api/user";
 
 export const userDataService = {
     createUser(data){
