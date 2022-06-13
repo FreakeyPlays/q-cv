e70aa103109861e435b18236501c5887a4d29f55
Leto01 <80838862+Leto01@users.noreply.github.com>
2022-06-13 09:42:34 +0200

    Added Titlebar to Dashboard, linked + Button to create-cv-page

--- client/src/pages/dashboard/Dashboard.js
+++ client/src/pages/dashboard/Dashboard.js
@@ -1,6 +1,13 @@
+import Titlebar from "../../components/titlebar/Titlebar";
+
 const Dashboard = () => {
     return(
         <>
+            <Titlebar 
+                searchbar={false} 
+                showAll={false}
+                path="/create-cv" 
+            />
         </>
     )
 }
