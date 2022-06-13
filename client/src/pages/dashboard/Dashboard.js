e9038b043ca8bfe2934619700c811a1496cd49dd
Leto01 <80838862+Leto01@users.noreply.github.com>
2022-06-13 11:34:44 +0200

    hook not working

--- client/src/pages/dashboard/Dashboard.js
+++ client/src/pages/dashboard/Dashboard.js
@@ -6,34 +6,28 @@ const Dashboard = () => {
 
     const dataDump = 0;
     const [userCvIdList, fillUserCvIdList] = useState([]); //list of object id's which are used to get the CVs of the current user.
-    const receivedData = useRef(False);
+    const receivedData = useRef(false);
     const [cvDataObjectList, setCvDataObjectList] = useState([]); //list of cv-objects fetched
 
     //for testing
     const getUserCvIds = ()=>{
-        //later: get id-list from user.
-        let fetchedIds = ['62a6f37507b0e590de6f1b04', '62a21e2c7e355fcfbece6dd1'] //test id's
-        fillUserCvIdList(fetchedIds) 
+        //later: get id-list from user
+        fillUserCvIdList(['62a6f37507b0e590de6f1b04', '62a21e2c7e355fcfbece6dd1']);
     }
 
     useEffect( ()=>{
         if(receivedData.current === false){
             getUserCvIds();
+            console.log(userCvIdList);
             userCvIdList.map( (item, index) =>{
                 //iterating over userCvIdList and fetching each CV-Obj with given id
                     cvDataService.get(item)                    
                         .then(response => setCvDataObjectList([...cvDataObjectList, response.data.response]))
                         .catch( e => console.error(e.message));
-                        
+                    console.log(cvDataObjectList[index].cvName);
                         dataDump = index;
-                } 
-                
+                }    
             )
-
-            
-
-
-            
             receivedData.current = true;
         }
     });
